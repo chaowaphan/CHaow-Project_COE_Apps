dd8936666c58401e95926358cf0ec265a1502af9
chaowaphan <5810110080@psu.ac.th>
2018-06-13 20:49:04 +0700

    Main Branch
    // Same Master

--- app/src/main/java/com/example/chaow/appexaminationtest/MainActivity.java
+++ app/src/main/java/com/example/chaow/appexaminationtest/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    // Home
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary341/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: D:\\ProjectApp_COE_Final\\AppExaminationTest\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: E:\\Github_Project\\CHaow-Project_COE_Apps\\openCVLibrary341\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
